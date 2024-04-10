cc780ba5c4e2b1363189585f6d361cae39f8f4c7
Defi1403 <144542367+Defi1403@users.noreply.github.com>
2024-04-10 12:17:51 +0300

    Add files via upload

---
+++ PZ_14/PZ_14_7.py
@@ -0,0 +1,13 @@
+# В исходном текстовом файле(Dostoevsky.txt) найти все годы деятельности писателя
+# (например, 1821 года, 1837 год, 1843 году и так далее по всему тексту). Посчитать количество полученных элементов.
+import re
+
+file = open('Dostoevsky.txt', 'r', encoding='utf-8')
+content = file.read()
+
+pattern = r'\b((?:18)[0-9]{2}–(?:18)[0-9]{2}|(?:18)[0-9]{2}\s)\b'
+matches = re.findall(pattern, content)
+
+count = len(matches)
+
+print(f"Количество лет деятельности писателя: {matches, count}")
