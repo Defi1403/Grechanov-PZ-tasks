62bf7d3f4c58d37b56ef3dabc4336d8e6eef63b0
Defi1403 <144542367+Defi1403@users.noreply.github.com>
2024-05-11 12:09:08 +0300

    Update PZ_7_2.py

--- PZ_7/PZ_7_2.py
+++ PZ_7/PZ_7_2.py
@@ -1,5 +1,5 @@
 # Дана строка, содержащая полное имя файла. Выделить из этой строки расширение файла (без предшествующей точки).
 
-string = 'C:\\Users\\Кирилл\\Documents\\Grechanov_IS_25\\PZ_7\\PZ_7_7(2).py'
+string = 'C:\\Users\\Кирилл\\Documents\\Grechanov_IS_25\\PZ_7\\PZ_7_2.py'
 
 print('Расширение файла:', string.split('.')[-1])
